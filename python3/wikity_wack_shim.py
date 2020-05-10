647810ffd908119934c156ca7f3dceac60bea53c
Adam Price <komidore64@gmail.com>
2020-05-09 21:50:37 -0400

    improve option handling
    - prompt if it's not saved in our vimrc
    - hide if it's our login password
    - error if you don't enter anything
    - cache the entered value for as long as vim is running

--- python3/wikity_wack_shim.py
+++ python3/wikity_wack_shim.py
@@ -6,11 +6,27 @@ class Shim():
     Shim is an adapter class for interfacing Wikity-Wack's python with Vim.
     """
 
-    def _get_config_options(self):
-        unsani = vim.vars['wikity_wack']
+    def _get_opt(self, opt_key, prompt=None, ask=True):
+       opt = vim.vars['wikity_wack'].get(opt_key, None)
 
-        sanitized = {k:unsani[k].decode() for k in self.options}
-        return sanitized
+       if not opt and ask:
+           opt = self._prompt(prompt or f"{opt_key}? : ", opt_key == 'password')
+           vim.vars['wikity_wack'][opt_key] = opt.encode()
+
+       if type(opt) is bytes:
+           opt = opt.decode()
+       return opt
+
+    def _prompt(self, prompt_msg, is_password=False):
+        try:
+            vim.eval('inputsave()')
+            cmd = 'inputsecret' if is_password else 'input'
+            ret = vim.eval(f"{cmd}('{self._squote_escape(prompt_msg)}', '')").strip()
+            if not len(ret):
+                raise Exception('Prompt input cannot be blank.')
+        finally:
+            vim.eval('inputrestore()')
+        return ret.strip()
 
     def _squote_escape(self, s):
         return s.replace("'", "''")
@@ -18,16 +34,16 @@ class Shim():
     # public
 
     def __init__(self):
-        self.options = [
-            'host',
-            'path',
-            'username',
-            'password',
-        ]
+        self.opts = dict(
+            host=self._get_opt('host', "Mediawiki hostname? : "),
+            username=self._get_opt('username', "Mediawiki username? : "),
+            password=self._get_opt('password', "Mediawiki password? : "),
+            path=self._get_opt('path', ask=False),
+        )
 
     def edit(self, article_name):
-        client = Client(**self._get_config_options())
+        client = Client(**self.opts)
 
         vim.current.buffer[:] = client.fetch_page(article_name).split("\n")
-        vim.command("set ft=mediawiki")
+        vim.command('set ft=mediawiki')
         vim.command(f"let b:article_name = '{self._squote_escape(article_name)}'")
